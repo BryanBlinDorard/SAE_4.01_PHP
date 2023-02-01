0cdce9a76d13fe1939d6f90bee42e2d1d448fed3
BLIN--DORARD Bryan <o22100568@info23-13.iut45.univ-orleans.fr>
2023-02-01 13:50:58 +0100

    ajout de la page home, menu, et questionnaire

---
+++ views/game.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Quyz</title>
+    </head>
+    
+    <body>
+        <style><?php include '../styles/home.css'; ?></style>
+        <style><?php include '../styles/game.css'; ?></style>
+        <div class="game">
+            <h2>Questionnaire 1</h2>
+            <div class="question">
+                <h3>Question 1</h3>
+
+            </div>
+        
+    </body>
+</html>
