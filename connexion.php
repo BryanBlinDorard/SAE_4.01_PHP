631ab6a2d2cc0c91bb175c95d80ba316f318381c
BryanBlinDorard <brybrybbd@gmail.com>
2023-02-02 14:20:07 +0100

    correction de la connexion

--- connexion.php
+++ connexion.php
@@ -4,8 +4,9 @@
     $dsn = "mysql:dbname=".BASE.";host=".SERVER;
     try {
         $connexion = new PDO($dsn, USER, PASSWORD);
-    } catch(PDOException $e) {
-        printf("Échec de la connexion : %s\n"; $e->getMessage());
+        echo "Connexion réussie";
+    } catch(PDOException $e){
+        printf("Échec de la connexion : %s\n", $e->getMessage());
         exit();
     }
 ?>
